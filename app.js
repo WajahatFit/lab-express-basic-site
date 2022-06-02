89ad4b886080e4d9a14e7dbf8ba605b9a165585b
Syed Wajahat Ali <wajahat.klk25.com>
2022-06-02 17:03:02 +0200

    made changes to /view and /app.js

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/',(req, res, next) => {
-    res.sendFile(__dirname + '/views/home');
+    res.sendFile(__dirname + '/views/home.html');
 } )
 
 app.get('/about',(req, res, next) => {
-    res.sendFile(__dirname + '/views/about');
+    res.sendFile(__dirname + '/views/about.html');
 } )
 
 app.get('/bonus',(req, res, next) => {
-    res.sendFile(__dirname + '/views/bonus');
+    res.sendFile(__dirname + '/views/bonus.html');
 } )
 
 app.get('/work',(req, res, next) => {
-    res.sendFile(__dirname + '/views/work');
+    res.sendFile(__dirname + '/views/work.html');
 } )
 console.log(__dirname);
 
 
 app.listen(3000, () => { console.log('Server running on port 3000 🛫') });
-
 
