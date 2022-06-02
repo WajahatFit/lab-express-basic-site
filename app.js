6a0df0466687d95e6b07eba635a7a0a7040bba62
Syed Wajahat Ali <wajahat.klk25.com>
2022-06-02 16:36:03 +0200

    changes on app.js

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/',(req, res, next) => {
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(__dirname + '/views/home');
 } )
 
 app.get('/about',(req, res, next) => {
-    res.sendFile(__dirname + '/views/about.html');
+    res.sendFile(__dirname + '/views/about');
 } )
 
 app.get('/bonus',(req, res, next) => {
-    res.sendFile(__dirname + '/views/bonus.html');
+    res.sendFile(__dirname + '/views/bonus');
 } )
 
 app.get('/work',(req, res, next) => {
-    res.sendFile(__dirname + '/views/work.html');
+    res.sendFile(__dirname + '/views/work');
 } )
 console.log(__dirname);
 
